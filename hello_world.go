38e305ebee2ae39bdd54a677d5a56d3c4928d9b4
Masataka Hisasue <masataka.hisasue@optim.co.jp>
2019-06-07 22:39:22 +0900

    feat: show image

--- hello_world.go
+++ hello_world.go
@@ -1,22 +1,33 @@
 package main
 
 import (
-	"image/color"
+	_ "image/png"
 	"log"
 
 	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
+
+var img *ebiten.Image
+
+func init() {
+	var err error
+	img, _, err = ebitenutil.NewImageFromFile("gopher.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func update(screen *ebiten.Image) error {
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
-	screen.Fill(color.RGBA{0xff, 0, 0, 0xff})
+	screen.DrawImage(img, nil)
 	return nil
 }
 
 func main() {
-	if err := ebiten.Run(update, 640, 480, 1, "Fill"); err != nil {
+	if err := ebiten.Run(update, 640, 480, 1, "Render an image"); err != nil {
 		log.Fatal(err)
 	}
 }
